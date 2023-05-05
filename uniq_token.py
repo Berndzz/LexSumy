4cc5569d227f62ea4a86d9d1c63a1de5af8bef75
Berndzz <tukanbernad@gmail.com>
2023-05-05 20:12:43 +0700

    ada

--- uniq_token.py
+++ uniq_token.py
@@ -1,13 +1,46 @@
+# import pandas as pd
+# import json
+
+# try:
+#     with open('util/label_mini_2.json') as f:
+#         data = json.load(f)
+#         read_json = pd.json_normalize(data)
+# except Exception as e:
+#     print(f"Error reading JSON file: {e}")
+#     read_json = pd.DataFrame()
+
+
+# def text_to_string(row):
+#     text = row['label']
+#     text = ','.join(' '.join(j['text'].title().split())
+#                     for j in text)  # .title() , .capitalize()
+#     return text
+
+
+# if not read_json.empty:
+#     read_json['lbl'] = read_json.apply(text_to_string, axis=1)
+#     read_json['lsl'] = read_json['lbl'].str.split(",")
+#     temp = read_json['lsl'].tolist()
+#     unique_token = []
+#     for doc in temp:
+#         for sentence in doc:
+#             unique_token.append(sentence)
+
+#     unique_token.append('Ajat Temuai Datai')
+#     unique_token.append('Dayak Mualang')
+#     unique_token.append('Dayak Iban')
+#     unique_token.append('Ki Naryo')
+#     unique_token.append('Tari Belian bawo')
+#     unique_token.append('Tari sajojo')
+# else:
+#     unique_token = []
 import pandas as pd
-import json
 
 try:
-    with open('util/label_mini_2.json') as f:
-        data = json.load(f)
-        read_json = pd.json_normalize(data)
-except Exception as e:
-    print(f"Error reading JSON file: {e}")
-    read_json = pd.DataFrame()
+    read_json = pd.read_json(r'util\label_mini_2.json')
+except FileNotFoundError:
+    print("File not found.")
+    # handle the exception as needed
 
 
 def text_to_string(row):
@@ -17,20 +50,10 @@ def text_to_string(row):
     return text
 
 
-if not read_json.empty:
-    read_json['lbl'] = read_json.apply(text_to_string, axis=1)
-    read_json['lsl'] = read_json['lbl'].str.split(",")
-    temp = read_json['lsl'].tolist()
-    unique_token = []
-    for doc in temp:
-        for sentence in doc:
-            unique_token.append(sentence)
+read_json['lbl'] = read_json.apply(text_to_string, axis=1)
+read_json['lsl'] = read_json['lbl'].str.split(",")
+temp = read_json['lsl'].tolist()
 
-    unique_token.append('Ajat Temuai Datai')
-    unique_token.append('Dayak Mualang')
-    unique_token.append('Dayak Iban')
-    unique_token.append('Ki Naryo')
-    unique_token.append('Tari Belian bawo')
-    unique_token.append('Tari sajojo')
-else:
-    unique_token = []
+unique_token = [sentence for doc in temp for sentence in doc]
+unique_token.extend(['Ajat Temuai Datai', 'Dayak Mualang',
+                    'Dayak Iban', 'Ki Naryo', 'Tari Belian bawo', 'Tari sajojo'])
